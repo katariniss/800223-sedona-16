73dea3e1ab807e41671cfe0de41ddfa8bef80e23
Katariniss <echekina@gmail.com>
2019-05-15 17:25:14 +0500

    Automation: svg sprite

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -60,4 +63,21 @@ gulp.task ("webp", function() {
   return gulp.src("source/img/**/*.{png, jpg}")
     .pipe(webp({quality: 90}))
     .pipe (gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg:true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 })
